aa6cc60c4535ae255c02470c757dbc8341ad170d
Thagoo <lohitgowda56@gmail.com>
2019-12-09 11:24:56 +0000

    rolex: Set DISABLE_EAP_PROXY

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -208,6 +208,7 @@ TARGET_DISABLE_WCNSS_CONFIG_COPY := true
 WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+DISABLE_EAP_PROXY := true
 
 # Inherit the common proprietary files
 -include vendor/xiaomi/rolex/BoardConfigVendor.mk
