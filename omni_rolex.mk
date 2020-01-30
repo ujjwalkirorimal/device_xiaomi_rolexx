52a578e2236c74208e00cc89861d298380b4ef33
Thagoo <lohitgowda56@gmail.com>
2020-01-30 03:51:10 +0000

    bring omnirom up
    Signed-off-by: Thagoo <lohitgowda56@gmail.com>

--- omni_rolex.mk
+++ omni_rolex.mk
@@ -20,8 +20,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Get the prebuilt list of APNs
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Inherit some common Omni stuff
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from rolex device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -30,7 +33,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := rolex
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi 4A
-PRODUCT_NAME := lineage_rolex
+PRODUCT_NAME := omni_rolex
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
