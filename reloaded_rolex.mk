ec3d74da740ba72aa88110381d31ddc41e37acc6
Thagoo <lohitgowda56@gmail.com>
2020-02-03 13:22:50 +0000

    rolex: bring reloaded caf up

--- reloaded_rolex.mk
+++ reloaded_rolex.mk
@@ -21,7 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/reloaded/common.mk)
 
 # Inherit from rolex device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -30,7 +30,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := rolex
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi 4A
-PRODUCT_NAME := lineage_rolex
+PRODUCT_NAME := reloaded_rolex
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
